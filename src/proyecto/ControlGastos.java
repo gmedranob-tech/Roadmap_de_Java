844c4758958458332493deb446d21fc110361195
Gabriela Medrano <gmedranob@miumg.edu.gt>
2026-06-07 22:13:56 -0600

    Proyecto - Implementacion inicial del sistema de control de gastos

--- src/proyecto/ControlGastos.java
+++ src/proyecto/ControlGastos.java
@@ -3,7 +3,20 @@ package proyecto;
 public class ControlGastos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Gasto gasto1 = new Gasto("Gasolina", 200);
+		Gasto gasto2 = new Gasto("Alimentacion", 150);
+
+		double total = gasto1.getMonto() + gasto2.getMonto();
+
+		System.out.println("===== CONTROL DE GASTOS =====");
+
+		System.out.println(gasto1.getDescripcion() + ": Q" + gasto1.getMonto());
+		System.out.println(gasto2.getDescripcion() + ": Q" + gasto2.getMonto());
+
+		System.out.println("----------------------------");
+
+		System.out.println("Total gastado: Q" + total);
 
 	}
 
---
+++ src/proyecto/Gasto.java
@@ -0,0 +1,27 @@
+package proyecto;
+
+public class Gasto {
+
+	private String descripcion;
+	private double monto;
+
+	public Gasto(String descripcion, double monto) {
+
+		this.descripcion = descripcion;
+		this.monto = monto;
+
+	}
+
+	public String getDescripcion() {
+
+		return descripcion;
+
+	}
+
+	public double getMonto() {
+
+		return monto;
+
+	}
+
+}
